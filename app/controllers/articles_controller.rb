19af49eb2e2fb8ccd266fe3fa576e9176f818fe2
TAmyG <tamy.vivas@gmail.com>
2016-01-10 15:47:33 -0600

    feature: creación de modelo HasCategory como tabla asociación muchos a muchos entre Article y Category, para la creación de artículos con múltiples categorías

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,11 +17,13 @@ class ArticlesController < ApplicationController
 	#GET /articles/new
 	def new
 		@article = Article.new
+		@categories = Category.all
 	end
 
 	#POST /articles
 	def create
 		@article = current_user.articles.new(article_params)
+		@article.categories = params[:categories]
 		if @article.valid?
 			@article.save
 			redirect_to @article
@@ -56,7 +58,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def article_params
-		params.require(:article).permit(:title, :body, :cover)
+		params.require(:article).permit(:title, :body, :cover, :categories)
 	end
 
 end
